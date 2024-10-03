9bcf9c4db9bae914fab8d12f29be6f84cab80fbe
unknown <kostya.mkhalkov@bk.ru>
2024-10-03 08:45:13 +0300

    feat: added functionality for Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 
+import { MyContext } from "./context/MyContext";
 import TodoApp from "./components/TodoApp/TodoApp";
 
 function App() {
+  const [state, setState] = useState("");
   return (
     <div className="App">
-      <TodoApp />
+      <MyContext.Provider value={{state, setState}}>
+        <TodoApp />
+      </MyContext.Provider>
     </div>
   );
 }
--- src/components/Headers/Headers.js
+++ src/components/Headers/Headers.js
@@ -1,10 +1,52 @@
+import { useState, useContext } from 'react';
+import uuid from 'react-uuid';
 
+import DatePicker from 'react-datepicker';
+import { MyContext } from '../../context/MyContext';
+
+import 'react-datepicker/dist/react-datepicker.css';
 import '../Headers/Headers.scss';
 
 const Headers = () => {
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [task, setTask] = useState("");
+    const {setState} = useContext(MyContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const regexp = /[A-Za-zА-Яа-я0-9.-]+/;
+        if (!regexp.test(task)) {
+            setTask("");
+            return;
+        }
+        setState(state => [...state, {task, id: uuid()}]);
+        setTask("");
+    };
+
     return (
-        <div className='header'>
-            <h1 className='header_title'>ToDo-list</h1>
+        <div className='header'
+             onSubmit={handleSubmit}>
+            <h1 className='header_title'>To Do list</h1>
+            <form className="task">
+                <label htmlFor="addTask"
+                       className="task_label">Add a new task</label>
+                <input type="text"
+                       name="addTask"
+                       required
+                       className="task_input"
+                       value={task}
+                       onChange={e => setTask(e.target.value)}/>
+                <button className="calendar"
+                        onClick={() => setShowDatePicker(!showDatePicker)}></button>
+                <button className="task_button"
+                        type="submit">
+                </button>
+                {showDatePicker ? <DatePicker 
+                          onChange={() => setShowDatePicker(false)}
+                          dateFormat="dd/MM/yyyy"
+                          placeholderText="Выберите дату завершения"/> : null}
+            </form>
         </div>
     )
 }
