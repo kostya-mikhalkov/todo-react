b2de6f8769461df83fb4d30b6392c919e30de5e5
unknown <kostya.mkhalkov@bk.ru>
2024-10-04 15:44:08 +0300

    feat:added Todoitem component

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,10 +1,57 @@
+import { useState, useContext } from 'react';
 
+import { MyContext } from '../../context/MyContext';
 import '../TodoItem/TodoItem.scss';
 
-const TodoItem = () => {
-    return (
-        <li className="todolist_item">
+const TodoItem = ({text, data, id}) => {
+    const [completed, setCompleted] = useState(false);
+    const [editTask, setEditTask] = useState(text);
+    const [edit, setEdit] = useState(false);
+    const {setState} = useContext(MyContext);
 
+    const editTaskChange = (e) => {
+        setEditTask(e.target.value);
+      };
+
+    const saveTask = () => {
+        setState((state) =>
+            state.map((item) =>
+            item.id === id ? { ...item, task: editTask } : item
+            )
+        );
+    };
+
+    const handleKey = (e) => {
+        if (e.key === 'Enter') {
+            saveTask()
+        }
+    }
+    
+
+    return (
+        <li className={`todolist_flex ${completed ? "completed" : null}`}
+            id={id}>
+            <label className="custom-checkbox">
+                <input type="checkbox" 
+                       checked={completed} 
+                       onChange={() => setCompleted(!completed)}/>
+                <span className="checkmark"></span>
+            </label>
+            {!edit ? <div className={`todolist_item_descr ${completed ? 'todolist_item_descr__complete' : null}`}>{text}</div> : 
+                     <input className='todolist_item_descr'
+                            onChange={(e) => editTaskChange(e)}
+                            onKeyDown={handleKey}/>}
+            <div className='todolist_item_date'>
+                <h6 className='todolist_item_date_descr'>{data ? 'End time' : 'No execution time'}</h6>
+                <span className='todolist_item_date_time'>{data}</span>
+            </div>
+            <button type="button"
+                    className='edit_btn'
+                    onClick={() => {
+                            saveTask()
+                            setEdit(!edit)}}></button>
+            <button type="button"
+                    className='delete_btn'></button>
         </li>
     )
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,14 +1,19 @@
+import { useContext } from 'react';
 
+import {MyContext} from '../../context/MyContext';
 import TodoItem from '../TodoItem/TodoItem';
 
 import './ToDoList.scss';
 
 const ToDolist = () => {
+    const {state} = useContext(MyContext);
     return (
         <div className="todolist">
-            <h2 className="todolist_title">Your tasks to complete</h2>
+            <h2 className="todolist_title">{state.length !== 0 ? 'Your tasks to complete' : null}</h2>
             <ul className='todolist_list'>
-                <TodoItem />
+                {state.length !== 0 ? state.map(item => {
+                    return <TodoItem text={item.task} data={item.data} id={item.id} key={item.id}/>
+                }) : null}
             </ul>
         </div>
     )
